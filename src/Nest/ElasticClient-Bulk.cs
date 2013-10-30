1c53595279888be7cf30168d5bb774f78f53a12d
Frank Heljebrandt <frank@heljebrandt.se>
2013-10-30 17:04:41 +0100

    master -> Temp

--- src/Nest/ElasticClient-Bulk.cs
+++ src/Nest/ElasticClient-Bulk.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Text.RegularExpressions;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,223 +11,222 @@ using System.Collections.Concurrent;
 
 namespace Nest
 {
-	public partial class ElasticClient
-	{
-		private Regex _bulkReplace = new Regex(@",\n|^\[", RegexOptions.Compiled | RegexOptions.Multiline);
+    public partial class ElasticClient
+    {
+        private Regex _bulkReplace = new Regex(@",\n|^\[", RegexOptions.Compiled | RegexOptions.Multiline);
 
-		public IBulkResponse Bulk(Func<BulkDescriptor, BulkDescriptor> bulkSelector)
-		{
-			bulkSelector.ThrowIfNull("bulkSelector");
-			var bulkDescriptor = bulkSelector(new BulkDescriptor());
-			return this.Bulk(bulkDescriptor);
-		}
-		public IBulkResponse Bulk(BulkDescriptor bulkDescriptor)
-		{
-			bulkDescriptor.ThrowIfNull("bulkDescriptor");
-			bulkDescriptor._Operations.ThrowIfEmpty("Bulk descriptor does not define any operations");
-			var sb = new StringBuilder();
-			
-			foreach (var operation in bulkDescriptor._Operations)
-			{
-				var command = operation._Operation;
-				var index = operation._Index ??
-				            bulkDescriptor._FixedIndex ?? 
-							new IndexNameResolver(this._connectionSettings).GetIndexForType(operation._ClrType);
-				var typeName = operation._Type
-				               ?? bulkDescriptor._FixedType
-				               ?? this.Infer.TypeName(operation._ClrType);
+        public IBulkResponse Bulk(Func<BulkDescriptor, BulkDescriptor> bulkSelector)
+        {
+            bulkSelector.ThrowIfNull("bulkSelector");
+            var bulkDescriptor = bulkSelector(new BulkDescriptor());
+            return this.Bulk(bulkDescriptor);
+        }
+        public IBulkResponse Bulk(BulkDescriptor bulkDescriptor)
+        {
+            bulkDescriptor.ThrowIfNull("bulkDescriptor");
+            bulkDescriptor._Operations.ThrowIfEmpty("Bulk descriptor does not define any operations");
+            var sb = new StringBuilder();
 
-				var id = operation.GetIdForObject(this.Infer);
-				operation._Index = index;
-				operation._Type = typeName;
-				operation._Id = id;
+            foreach (var operation in bulkDescriptor._Operations)
+            {
+                var command = operation._Operation;
+                var index = operation._Index ??
+                            bulkDescriptor._FixedIndex ??
+                            new IndexNameResolver(this._connectionSettings).GetIndexForType(operation._ClrType);
+                var typeName = operation._Type
+                               ?? bulkDescriptor._FixedType
+                               ?? this.Infer.TypeName(operation._ClrType);
 
-				var opJson = this.Serializer.Serialize(operation, Formatting.None);
+                var id = operation.GetIdForObject(this.Infer);
+                operation._Index = index;
+                operation._Type = typeName;
+                operation._Id = id;
 
-				var action = "{{ \"{0}\" :  {1} }}\n".F(command, opJson);
-				sb.Append(action);
+                var opJson = this.Serializer.Serialize(operation, Formatting.None);
 
-				if (command == "index" || command == "create")
-				{
-					string jsonCommand = this.Serializer.Serialize(operation._Object, Formatting.None);
-					sb.Append(jsonCommand + "\n");
-				}
-				else if (command == "update")
-				{
-					string jsonCommand = this.Serializer.Serialize(operation.GetBody(), Formatting.None);
-					sb.Append(jsonCommand + "\n");
-				}
-			}
-			var json = sb.ToString();
-			var path = "_bulk";
-			if (!bulkDescriptor._FixedIndex.IsNullOrEmpty())
-			{
-				if (!bulkDescriptor._FixedType.IsNullOrEmpty())
-					path = bulkDescriptor._FixedType + "/" + path;
-				path = bulkDescriptor._FixedIndex + "/" + path;
-			}
-			var status = this.Connection.PostSync(path, json);
-			return this.Deserialize<BulkResponse>(status);
-		}
-		
-		internal string GenerateBulkIndexCommand<T>(IEnumerable<T> objects) where T : class
-		{
-			return this.GenerateBulkCommand<T>(@objects, "index");
-		}
-		private string GenerateBulkIndexCommand<T>(IEnumerable<BulkParameters<T>> objects) where T : class
-		{
-			return this.GenerateBulkCommand<T>(@objects, "index");
-		}
-		private string GenerateBulkIndexCommand<T>(IEnumerable<T> objects, string index) where T : class
-		{
-			return this.GenerateBulkCommand<T>(@objects, index, "index");
-		}
-		private string GenerateBulkIndexCommand<T>(IEnumerable<BulkParameters<T>> objects, string index) where T : class
-		{
-			return this.GenerateBulkCommand<T>(@objects, index, "index");
-		}
-		private string GenerateBulkIndexCommand<T>(IEnumerable<T> @objects, string index, string typeName) where T : class
-		{
-			return this.GenerateBulkCommand<T>(@objects, index, typeName, "index");
-		}
-		private string GenerateBulkIndexCommand<T>(IEnumerable<BulkParameters<T>> @objects, string index, string typeName) where T : class
-		{
-			return this.GenerateBulkCommand<T>(@objects, index, typeName, "index");
-		}
+                var action = "{{ \"{0}\" :  {1} }}\n".F(command, opJson);
+                sb.Append(action);
 
-		private string GenerateBulkDeleteCommand<T>(IEnumerable<T> objects) where T : class
-		{
-			return this.GenerateBulkCommand<T>(@objects, "delete");
-		}
-		private string GenerateBulkDeleteCommand<T>(IEnumerable<BulkParameters<T>> objects) where T : class
-		{
-			return this.GenerateBulkCommand<T>(@objects, "delete");
-		}
-		private string GenerateBulkDeleteCommand<T>(IEnumerable<T> objects, string index) where T : class
-		{
-			return this.GenerateBulkCommand<T>(@objects, index, "delete");
-		}
-		private string GenerateBulkDeleteCommand<T>(IEnumerable<BulkParameters<T>> objects, string index) where T : class
-		{
-			return this.GenerateBulkCommand<T>(@objects, index, "delete");
-		}
-		private string GenerateBulkDeleteCommand<T>(IEnumerable<T> @objects, string index, string typeName) where T : class
-		{
-			return this.GenerateBulkCommand<T>(@objects, index, typeName, "delete");
-		}
-		private string GenerateBulkDeleteCommand<T>(IEnumerable<BulkParameters<T>> @objects, string index, string typeName) where T : class
-		{
-			return this.GenerateBulkCommand<T>(@objects, index, typeName, "delete");
-		}
+                if (command == "index" || command == "create")
+                {
+                    string jsonCommand = this.Serializer.Serialize(operation._Object, Formatting.None);
+                    sb.Append(jsonCommand + "\n");
+                }
+                else if (command == "update")
+                {
+                    string jsonCommand = this.Serializer.Serialize(operation.GetBody(), Formatting.None);
+                    sb.Append(jsonCommand + "\n");
+                }
+            }
+            var json = sb.ToString();
+            var path = "_bulk";
+            if (!bulkDescriptor._FixedIndex.IsNullOrEmpty())
+            {
+                if (!bulkDescriptor._FixedType.IsNullOrEmpty())
+                    path = bulkDescriptor._FixedType + "/" + path;
+                path = bulkDescriptor._FixedIndex + "/" + path;
+            }
+            var status = this.Connection.PostSync(path, json);
+            return this.Deserialize<BulkResponse>(status);
+        }
 
-		private string GenerateBulkCommand<T>(IEnumerable<T> objects, string command) where T : class
-		{
-			objects.ThrowIfEmpty("objects");
+        internal string GenerateBulkIndexCommand<T>(IEnumerable<T> objects) where T : class
+        {
+            return this.GenerateBulkCommand<T>(@objects, "index");
+        }
+        private string GenerateBulkIndexCommand<T>(IEnumerable<BulkParameters<T>> objects) where T : class
+        {
+            return this.GenerateBulkCommand<T>(@objects, "index");
+        }
+        private string GenerateBulkIndexCommand<T>(IEnumerable<T> objects, string index) where T : class
+        {
+            return this.GenerateBulkCommand<T>(@objects, index, "index");
+        }
+        private string GenerateBulkIndexCommand<T>(IEnumerable<BulkParameters<T>> objects, string index) where T : class
+        {
+            return this.GenerateBulkCommand<T>(@objects, index, "index");
+        }
+        private string GenerateBulkIndexCommand<T>(IEnumerable<T> @objects, string index, string typeName) where T : class
+        {
+            return this.GenerateBulkCommand<T>(@objects, index, typeName, "index");
+        }
+        private string GenerateBulkIndexCommand<T>(IEnumerable<BulkParameters<T>> @objects, string index, string typeName) where T : class
+        {
+            return this.GenerateBulkCommand<T>(@objects, index, typeName, "index");
+        }
 
-			var index = this.Infer.IndexName<T>();
-			if (string.IsNullOrEmpty(index))
-				throw new NullReferenceException("Cannot infer default index for current connection.");
+        private string GenerateBulkDeleteCommand<T>(IEnumerable<T> objects) where T : class
+        {
+            return this.GenerateBulkCommand<T>(@objects, "delete");
+        }
+        private string GenerateBulkDeleteCommand<T>(IEnumerable<BulkParameters<T>> objects) where T : class
+        {
+            return this.GenerateBulkCommand<T>(@objects, "delete");
+        }
+        private string GenerateBulkDeleteCommand<T>(IEnumerable<T> objects, string index) where T : class
+        {
+            return this.GenerateBulkCommand<T>(@objects, index, "delete");
+        }
+        private string GenerateBulkDeleteCommand<T>(IEnumerable<BulkParameters<T>> objects, string index) where T : class
+        {
+            return this.GenerateBulkCommand<T>(@objects, index, "delete");
+        }
+        private string GenerateBulkDeleteCommand<T>(IEnumerable<T> @objects, string index, string typeName) where T : class
+        {
+            return this.GenerateBulkCommand<T>(@objects, index, typeName, "delete");
+        }
+        private string GenerateBulkDeleteCommand<T>(IEnumerable<BulkParameters<T>> @objects, string index, string typeName) where T : class
+        {
+            return this.GenerateBulkCommand<T>(@objects, index, typeName, "delete");
+        }
 
-			return this.GenerateBulkCommand<T>(objects, index, command);
-		}
-		private string GenerateBulkCommand<T>(IEnumerable<BulkParameters<T>> objects, string command) where T : class
-		{
-			objects.ThrowIfEmpty("objects");
+        private string GenerateBulkCommand<T>(IEnumerable<T> objects, string command) where T : class
+        {
+            objects.ThrowIfEmpty("objects");
 
-			var index = this.Infer.IndexName<T>();
-			if (string.IsNullOrEmpty(index))
-				throw new NullReferenceException("Cannot infer default index for current connection.");
+            var index = this.Infer.IndexName<T>();
+            if (string.IsNullOrEmpty(index))
+                throw new NullReferenceException("Cannot infer default index for current connection.");
 
-			return this.GenerateBulkCommand<T>(objects, index, command);
-		}
-		private string GenerateBulkCommand<T>(IEnumerable<T> objects, string index, string command) where T : class
-		{
-			objects.ThrowIfEmpty("objects");
-			index.ThrowIfNullOrEmpty("index");
+            return this.GenerateBulkCommand<T>(objects, index, command);
+        }
+        private string GenerateBulkCommand<T>(IEnumerable<BulkParameters<T>> objects, string command) where T : class
+        {
+            objects.ThrowIfEmpty("objects");
 
-			var type = typeof(T);
-			var typeName = this.Infer.TypeName<T>();
+            var index = this.Infer.IndexName<T>();
+            if (string.IsNullOrEmpty(index))
+                throw new NullReferenceException("Cannot infer default index for current connection.");
 
-			return this.GenerateBulkCommand<T>(objects, index, typeName, command);
-		}
-		private string GenerateBulkCommand<T>(IEnumerable<BulkParameters<T>> objects, string index, string command) where T : class
-		{
-			objects.ThrowIfEmpty("objects");
-			index.ThrowIfNullOrEmpty("index");
+            return this.GenerateBulkCommand<T>(objects, index, command);
+        }
+        private string GenerateBulkCommand<T>(IEnumerable<T> objects, string index, string command) where T : class
+        {
+            objects.ThrowIfEmpty("objects");
+            index.ThrowIfNullOrEmpty("index");
 
-			var type = typeof(T);
-			var typeName = this.Infer.TypeName<T>();
+            var type = typeof(T);
+            var typeName = this.Infer.TypeName<T>();
 
-			return this.GenerateBulkCommand<T>(objects, index, typeName, command);
-		}
+            return this.GenerateBulkCommand<T>(objects, index, typeName, command);
+        }
+        private string GenerateBulkCommand<T>(IEnumerable<BulkParameters<T>> objects, string index, string command) where T : class
+        {
+            objects.ThrowIfEmpty("objects");
+            index.ThrowIfNullOrEmpty("index");
 
+            var type = typeof(T);
+            var typeName = this.Infer.TypeName<T>();
 
-		private string GenerateBulkCommand<T>(IEnumerable<T> @objects, string index, string typeName, string command) where T : class
-		{
-			objects.ThrowIfEmpty("objects");
-
-			var sb = new StringBuilder();
-			var action = "{{ \"{0}\" : {{ \"_index\" : \"{1}\", \"_type\" : \"{2}\"".F(command, index, typeName);
-
-			foreach (var @object in objects)
-			{
-				var objectAction = action;
-				
-					var id = this.Infer.Id(@object);
-					if (!id.IsNullOrEmpty())
-						objectAction += ", \"_id\" : \"{0}\" ".F(id);
-
-				objectAction += "} }\n";
-
-				sb.Append(objectAction);
-				if (command == "index")
-				{
-					string jsonCommand = this.Serializer.Serialize(@object, Formatting.None);
-					sb.Append(jsonCommand + "\n");
-				}
-			}
-			var json = sb.ToString();
-			return json;
+            return this.GenerateBulkCommand<T>(objects, index, typeName, command);
+        }
 
 
+        private string GenerateBulkCommand<T>(IEnumerable<T> @objects, string index, string typeName, string command) where T : class
+        {
+            objects.ThrowIfEmpty("objects");
+            using (var sw = new StringWriter())
+            using (var jw = new JsonTextWriter(sw))
+            {
 
-		}
-		private string GenerateBulkCommand<T>(IEnumerable<BulkParameters<T>> @objects, string index, string typeName, string command) where T : class
-		{
-			objects.ThrowIfEmpty("objects");
+                foreach (var @object in objects)
+                {
+                    var id = this.Infer.Id(@object);
+                    jw.WriteStartObject();
+                        jw.WritePropertyName(command);
+                        jw.WriteStartObject();
+                            jw.WritePropertyName("_index"); jw.WriteValue(index);
+                            jw.WritePropertyName("_type"); jw.WriteValue(typeName);
+                            if (!id.IsNullOrEmpty())
+                                jw.WritePropertyName("_id"); jw.WriteValue(id);
+                        jw.WriteEndObject();
+                    jw.WriteEndObject();
+                    jw.WriteRaw("\n");
+                    if (command == "index")
+                    {
+                        this.Serializer.SerializeStream(jw, @object, Formatting.None);
+                        jw.WriteRaw("\n");
+                    }
+                }
+                return sw.ToString();
+            }
+        }
+        private string GenerateBulkCommand<T>(IEnumerable<BulkParameters<T>> @objects, string index, string typeName, string command) where T : class
+        {
+            objects.ThrowIfEmpty("objects");
 
-			var sb = new StringBuilder();
-			var action = "{{ \"{0}\" : {{ \"_index\" : \"{1}\", \"_type\" : \"{2}\"".F(command, index, typeName);
+            var sb = new StringBuilder();
+            var action = "{{ \"{0}\" : {{ \"_index\" : \"{1}\", \"_type\" : \"{2}\"".F(command, index, typeName);
 
-			foreach (var @object in objects)
-			{
-				if (@object.Document == null)
-					continue;
+            foreach (var @object in objects)
+            {
+                if (@object.Document == null)
+                    continue;
 
-				var objectAction = action;
+                var objectAction = action;
 
-				objectAction += ", \"_id\" : \"{0}\" ".F(this.Infer.Id(@object.Document));
+                objectAction += ", \"_id\" : \"{0}\" ".F(this.Infer.Id(@object.Document));
 
-				if (!@object.Version.IsNullOrEmpty())
-					objectAction += ", \"version\" : \"{0}\" ".F(@object.Version);
-				if (!@object.Parent.IsNullOrEmpty())
-					objectAction += ", \"parent\" : \"{0}\" ".F(@object.Parent);
-				if (@object.VersionType != VersionType.Internal)
-					objectAction += ", \"version_type\" : \"{0}\" ".F(@object.VersionType.ToString().ToLower());
-				if (!@object.Routing.IsNullOrEmpty())
-					objectAction += ", \"routing\" : \"{0}\" ".F(@object.Routing);
-				objectAction += "} }\n";
+                if (!@object.Version.IsNullOrEmpty())
+                    objectAction += ", \"version\" : \"{0}\" ".F(@object.Version);
+                if (!@object.Parent.IsNullOrEmpty())
+                    objectAction += ", \"parent\" : \"{0}\" ".F(@object.Parent);
+                if (@object.VersionType != VersionType.Internal)
+                    objectAction += ", \"version_type\" : \"{0}\" ".F(@object.VersionType.ToString().ToLower());
+                if (!@object.Routing.IsNullOrEmpty())
+                    objectAction += ", \"routing\" : \"{0}\" ".F(@object.Routing);
+                objectAction += "} }\n";
 
-				sb.Append(objectAction);
-				if (command == "index")
-				{
-					string jsonCommand = this.Serializer.Serialize(@object.Document, Formatting.None);
-					sb.Append(jsonCommand + "\n");
-				}
-			}
-			var json = sb.ToString();
-			return json;
-		}
+                sb.Append(objectAction);
+                if (command == "index")
+                {
+                    string jsonCommand = this.Serializer.Serialize(@object.Document, Formatting.None);
+                    sb.Append(jsonCommand + "\n");
+                }
+            }
+            var json = sb.ToString();
+            return json;
+        }
 
-	}
+    }
 }
--- src/Nest/ExposedInternals/ElasticSerializer.cs
+++ src/Nest/ExposedInternals/ElasticSerializer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using Nest.Resolvers;
@@ -15,8 +16,8 @@ namespace Nest
 		private readonly IConnectionSettings _settings;
 		private readonly PropertyNameResolver _propertyNameResolver;
 		private readonly JsonSerializerSettings _serializationSettings;
+        private readonly JsonSerializer _serializer;
 
-		private static readonly ConcurrentBag<JsonConverter> _extraConverters = new ConcurrentBag<JsonConverter>();
 		private static readonly ConcurrentBag<JsonConverter> _defaultConverters = new ConcurrentBag<JsonConverter>
 		{
 			new IsoDateTimeConverter(),
@@ -24,18 +25,36 @@ namespace Nest
 			new DictionaryKeysAreNotPropertyNamesJsonConverter()
 		};
 
-		public ElasticSerializer(IConnectionSettings settings)
+
+	    public ElasticSerializer(IConnectionSettings settings)
 		{
 			this._settings = settings;
 			this._serializationSettings = this.CreateSettings();
+		    this._serializer = this.CreateSerializer();
 			this._propertyNameResolver = new PropertyNameResolver();
 		}
 
-		/// <summary>
+	    private JsonSerializer CreateSerializer()
+	    {
+	        var serializer = new JsonSerializer
+	                         {
+	                             ContractResolver = new ElasticContractResolver(this._settings),
+	                             NullValueHandling = NullValueHandling.Ignore,
+	                             DefaultValueHandling = DefaultValueHandling.Include
+	                         };
+	        serializer.Converters.Add(new IsoDateTimeConverter());
+	        serializer.Converters.Add(new FacetConverter());
+	        serializer.Converters.Add(new DictionaryKeysAreNotPropertyNamesJsonConverter());
+
+	        return serializer;
+	    }
+
+	    /// <summary>
 		/// Allows you to adjust the buildin JsonSerializerSettings to your liking
 		/// </summary>
 		public void ModifyJsonSerializationSettings(Action<JsonSerializerSettings> modifier)
 		{
+            // Todo: Fix this correctly
 			modifier(this._serializationSettings);
 		}
 			
@@ -45,7 +64,7 @@ namespace Nest
 		public void AddConverter(JsonConverter converter)
 		{
 			this._serializationSettings.Converters.Add(converter);
-			_extraConverters.Add(converter);
+            this._serializer.Converters.Add(converter);
 		}
 
 		/// <summary>
@@ -60,9 +79,9 @@ namespace Nest
 					|| status.Error.HttpStatusCode == System.Net.HttpStatusCode.NotFound)
 				: (status.Error == null);
 			var r = (R)Activator.CreateInstance(typeof(R));
-      var baseResponse = r as BaseResponse;
-      if (baseResponse == null)
-        return null;
+            var baseResponse = r as BaseResponse;
+            if (baseResponse == null)
+                return null;
 
 			baseResponse.IsValid = isValid;
 			baseResponse.ConnectionStatus = status;
@@ -84,49 +103,76 @@ namespace Nest
 				return this.ToResponse<R>(status, allow404);
 
 			var r = this.Deserialize<R>(status.Result, extraConverters: extraConverters);
-      var baseResponse = r as BaseResponse;
-      if (baseResponse == null)
-        return null;
+            var baseResponse = r as BaseResponse;
+            if (baseResponse == null)
+                return null;
 			baseResponse.IsValid = isValid;
 			baseResponse.ConnectionStatus = status;
 			baseResponse.PropertyNameResolver = this._propertyNameResolver;
 			return r;
 		}
 
-		/// <summary>
-		/// serialize an object using the internal registered converters without camelcasing properties as is done 
-		/// while indexing objects
-		/// </summary>
-		public string Serialize(object @object, Formatting formatting = Formatting.Indented)
-		{
-			return JsonConvert.SerializeObject(@object, formatting, this._serializationSettings);
-		}
+        /// <summary>
+        /// serialize an object using the internal registered converters without camelcasing properties as is done 
+        /// while indexing objects
+        /// </summary>
+        public string Serialize(object @object, Formatting formatting = Formatting.Indented)
+        {
+            using (var sw = new StringWriter())
+            using (var jw = new JsonTextWriter(sw))
+            {
+                this._serializer.Formatting = formatting;
+                this._serializer.Serialize(jw, @object);
+                return sw.ToString();
+            }
+        }
 
-		/// <summary>
+        /// <summary>
+        /// serialize an object using the internal registered converters without camelcasing properties as is done 
+        /// while indexing objects
+        /// </summary>
+        public void SerializeStream(JsonTextWriter writer, object @object, Formatting formatting = Formatting.Indented)
+        {
+            this._serializer.Formatting = formatting;
+            this._serializer.Serialize(writer, @object);
+        }
+
+        /// <summary>
 		/// Deserialize an object 
 		/// </summary>
 		/// <param name="notFoundIsValid">When deserializing a ConnectionStatus to a BaseResponse this controls whether a 404 is a valid response</param>
 		public T Deserialize<T>(object value, IEnumerable<JsonConverter> extraConverters = null, bool notFoundIsValidResponse = false) where T : class
 		{
       
-			var settings = this._serializationSettings;
 			if (extraConverters.HasAny())
 			{
-				settings = this.CreateSettings();
 				var concrete = extraConverters.OfType<ConcreteTypeConverter>().FirstOrDefault();
-				if (concrete != null)
-				{
-					((ElasticContractResolver)settings.ContractResolver).ConcreteTypeConverter = concrete;
-				}
-				else
-					settings.Converters = settings.Converters.Concat(extraConverters).ToList();
+                if (concrete != null)
+                {
+                    ((ElasticContractResolver) _serializer.ContractResolver).ConcreteTypeConverter = concrete;
+                }
+                else
+                {
+                    foreach (var extraConverter in extraConverters)
+                    {
+                        _serializer.Converters.Add(extraConverter);
+                    }
+                }
 
 			}
-      var status = value as ConnectionStatus;
-      if (status == null || !typeof(BaseResponse).IsAssignableFrom(typeof(T)))
-        return JsonConvert.DeserializeObject<T>(value.ToString(), settings);
+            var status = value as ConnectionStatus;
+            if (status == null || !typeof (BaseResponse).IsAssignableFrom(typeof (T)))
+            {
+                using (var sr = new StringReader(value.ToString()))
+                using (var jr = new JsonTextReader(sr))
+                {
+                    // TODO: Handle extra converters
+                    return this._serializer.Deserialize<T>(jr);
+                }
+            }
+                //return JsonConvert.DeserializeObject<T>(value.ToString(), settings);
 
-      return this.ToParsedResponse<T>(status, notFoundIsValidResponse, extraConverters);
+            return this.ToParsedResponse<T>(status, notFoundIsValidResponse, extraConverters);
 
 		}
 		private JsonSerializerSettings CreateSettings()
@@ -136,7 +182,7 @@ namespace Nest
 				ContractResolver = new ElasticContractResolver(this._settings),
 				NullValueHandling = NullValueHandling.Ignore,
 				DefaultValueHandling = DefaultValueHandling.Include,
-				Converters = _defaultConverters.Concat(_extraConverters).ToList()
+				Converters = _defaultConverters.ToList()
 			};
 		}
 	}
